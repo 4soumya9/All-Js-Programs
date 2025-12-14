07c66413c2223a4b73b214d1dcdd66c838ca5ef1
Soumyajit <4soumya19@gmail.com>
2025-12-14 14:29:02 +0530

    firstProblem

--- StringProblems/q1.js
+++ StringProblems/q1.js
@@ -1,1 +1,16 @@
 // reverse a string
+// const input = "  i like   this program very much  ";
+// console.log(reversedSen(input));
+
+let input = "  i like   this program very much  ";
+function rev(input) {
+  let res = "";
+  let words = input.split(" ").filter((t) => t.length > 0);
+  let stack = [...words];
+  while (stack.length > 0) {
+    let ele = stack.pop();
+    res += ele + " ";
+  }
+  return res.trim();
+}
+console.log(rev(input));
